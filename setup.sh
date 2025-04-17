a4c007fc8cf7a7d1382ef5eb23dd45f8796a1fcf
Jakob Danninger <68190922+jdanninger@users.noreply.github.com>
2025-04-17 12:35:46 -0400

    new stuff

--- setup.sh
+++ setup.sh
@@ -1,14 +1,8 @@
-
 mkdir -p ~/.streamlit/
-
-echo "\
-[general]\n\
-email = \"jkd50@case.edu\"\n\
-" > ~/.streamlit/credentials.toml
-
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
+echo "
+[server]n
+headless = truen
+port = $PORTn
+enableCORS = falsen
+n
 " > ~/.streamlit/config.toml
